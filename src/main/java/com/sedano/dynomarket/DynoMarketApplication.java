db5df2da3f6ce90b7c740a440ee5ad26dcf902db
Dynotum <sedano.carlos93@gmail.com>
2020-09-10 22:37:04 -0500

    First commit

---
+++ src/main/java/com/sedano/dynomarket/DynoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sedano.dynomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedano/dynomarket/DynoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedano.dynomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
