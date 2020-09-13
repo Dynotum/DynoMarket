2c0a9c2258f13aa7f66dc79f1be7d906d6d82cf6
dynotum <dino_arquero_1@hotmail.com>
2020-09-12 23:51:46 -0500

    Added Persistance class

---
+++ src/main/java/com/sedano/dynomarket/persistence/entity/Producto.java
@@ -0,0 +1,16 @@
+package com.sedano.dynomarket.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+
+    // name
+}
